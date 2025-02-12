108eb4391e145fd07d74229f20238662e56ab4d9
itsgabeover <gabe.enrique.miranda@gmail.com>
2025-02-12 12:19:15 -0500

    convert string

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import { getLatestProducts } from "@/lib/actions/product.actions";
 const Homepage = async() => {
 
   const latestProducts = await getLatestProducts();
+  console.log(latestProducts)
   return (
     <>
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4}/>
